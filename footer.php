26e850edaea03e783f1386645c97a804b305d9f9
AradhyaInternational <132771155+AradhyaInternational@users.noreply.github.com>
2023-09-10 18:52:44 +0530

    lknn
    m n

--- footer.php
+++ footer.php
@@ -6,7 +6,9 @@
             <div class="col-md-12 mb-5 text-center">
                 <div class="d-flex  justify-content-center justify-content-lg-start">
                     <div title="Triac" class="text-lg-start main-foot-box">
-                        <a style="font-size:2.3em ;" class="navbar-brand" href="index.php"><img class="navbrand-foot-img" src="images/TRIAC ELECTRONICS logo (002).png" alt=""> Triac Electronics Pvt Ltd</a>
+                        <a style="font-size:2.3em ;" class="navbar-brand" href="index.php"><img
+                                class="navbrand-foot-img" src="images/TRIAC ELECTRONICS logo (002).png" alt=""> Triac
+                            Electronics Pvt Ltd</a>
                         <p class="text-light main-foot-p">Bridging The Gaps to Make Real Designs.</p>
                     </div>
                 </div>
@@ -14,15 +16,22 @@
             <div class="col-md-12 pt-3 my-lg-5 mt-0">
                 <div class="row text-left ">
                     <div class="col-lg-3">
-                    <ul class="list-unstyled mb-0">
-                            <li class=""><a href="#!" class="d-flex fs-4 justify-content-center justify-content-lg-start mb-4 text-light">Company</a>
+                        <ul class="list-unstyled mb-0">
+                            <li class=""><a href="#!"
+                                    class="d-flex fs-4 justify-content-center justify-content-lg-start mb-4 text-light">Company</a>
 
-                                <ul class="align-items-center align-items-lg-start d-flex flex-column justify-content-center list-unstyled wb-ul">
-                                    <li id="menu-item-654" class=""><a href="about.php" class="mb-3 d-block text-secondary fw-semibold">About</a></li>
-                                    <li id="menu-item-656" class=""><a href="product.php" class="mb-3 d-block text-secondary fw-semibold">Our Products</a></li>
-                                    <li id="menu-item-655" class=""><a href="quality.php" class="mb-3 d-block text-secondary fw-semibold">Quality</a></li>
-                                    <li id="menu-item-659" class=""><a href="brands.php" class="mb-3 d-block text-secondary fw-semibold">Brands</a></li>
-                                     <li id="menu-item-659" class=""><a href="return_policy.php" class="mb-3 d-block text-secondary fw-semibold">Return Policy</a></li>
+                                <ul
+                                    class="align-items-center align-items-lg-start d-flex flex-column justify-content-center list-unstyled wb-ul">
+                                    <li id="menu-item-654" class=""><a href="about.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">About</a></li>
+                                    <li id="menu-item-656" class=""><a href="product.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Our Products</a></li>
+                                    <li id="menu-item-655" class=""><a href="quality.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Quality</a></li>
+                                    <li id="menu-item-659" class=""><a href="brands.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Brands</a></li>
+                                    <li id="menu-item-659" class=""><a href="return_policy.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Return Policy</a></li>
 
                                 </ul>
                             </li>
@@ -30,17 +39,25 @@
 
                     </div>
                     <div class="col-lg-3">
-                        
-                        <ul class="list-unstyled  mb-0">
-                            <li class=""><a href="#!" class="d-flex d-flex fs-4 justify-content-center justify-content-lg-start mb-4 text-light">Services</a>
 
-                                <ul class="align-items-center align-items-lg-start d-flex flex-column justify-content-center list-unstyled wb-ul">
-                                    <li id="menu-item-656" class=""><a href="newrnd.php" class="mb-3 d-block text-secondary fw-semibold">Research and Development</a></li>
-                                    <li id="menu-item-655" class=""><a href="scs.php" class="mb-3 d-block text-secondary fw-semibold">Electronic manufacturing </a></li>
-                                    <li id="menu-item-659" class=""><a href="eim.php" class="mb-3 d-block text-secondary fw-semibold">Excess inventory management
+                        <ul class="list-unstyled  mb-0">
+                            <li class=""><a href="#!"
+                                    class="d-flex d-flex fs-4 justify-content-center justify-content-lg-start mb-4 text-light">Services</a>
+
+                                <ul
+                                    class="align-items-center align-items-lg-start d-flex flex-column justify-content-center list-unstyled wb-ul">
+                                    <li id="menu-item-656" class=""><a href="newrnd.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Research and Development</a>
+                                    </li>
+                                    <li id="menu-item-655" class=""><a href="scs.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Electronic manufacturing
                                         </a></li>
-                                         <li id="menu-item-659" class=""><a href="ecs.php" class="mb-3 d-block text-secondary fw-semibold">Electronic Component <br>
-                                         Sourcing
+                                    <li id="menu-item-659" class=""><a href="eim.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Excess inventory management
+                                        </a></li>
+                                    <li id="menu-item-659" class=""><a href="ecs.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Electronic Component <br>
+                                            Sourcing
                                         </a></li>
                                 </ul>
                             </li>
@@ -48,13 +65,20 @@
                     </div>
                     <div class="col-lg-3">
                         <ul class="list-unstyled  mb-0">
-                            <li class=""><a href="blogs.php" class="d-flex d-flex fs-4 justify-content-center justify-content-lg-start mb-4 text-light">Blogs</a>
+                            <li class=""><a href="blogs.php"
+                                    class="d-flex d-flex fs-4 justify-content-center justify-content-lg-start mb-4 text-light">Blogs</a>
 
-                                <ul class="align-items-center align-items-lg-start d-flex flex-column justify-content-center list-unstyled wb-ul">
-                                    <li id="menu-item-654" class=""><a href="blogs.php" class="mb-3 d-block text-secondary fw-semibold">Industry Trends & <br> Insight</a></li>
-                                    <li id="menu-item-656" class=""><a href="blogs.php" class="mb-3 d-block text-secondary fw-semibold">Manufacturers news</a></li>
-                                    <li id="menu-item-655" class=""><a href="blogs.php" class="mb-3 d-block text-secondary fw-semibold">Events</a></li>
-                                    <li id="menu-item-659" class=""><a href="blogs.php" class="mb-3 d-block text-secondary fw-semibold">Exhibitions</a></li>
+                                <ul
+                                    class="align-items-center align-items-lg-start d-flex flex-column justify-content-center list-unstyled wb-ul">
+                                    <li id="menu-item-654" class=""><a href="blogs.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Industry Trends & <br>
+                                            Insight</a></li>
+                                    <li id="menu-item-656" class=""><a href="blogs.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Manufacturers news</a></li>
+                                    <li id="menu-item-655" class=""><a href="blogs.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Events</a></li>
+                                    <li id="menu-item-659" class=""><a href="blogs.php"
+                                            class="mb-3 d-block text-secondary fw-semibold">Exhibitions</a></li>
 
                                 </ul>
                             </li>
@@ -63,11 +87,14 @@
                     <div class="col-lg-3 d-flex justify-content-lg-end justify-content-center">
                         <div class="d-flex flex-column-reverse text-right">
                             <div class="follow-us py-5">
-                                <p class="d-inline-flex justify-content-center justify-content-lg-start text-light w-100">Follow us</p>
+                                <p
+                                    class="d-inline-flex justify-content-center justify-content-lg-start text-light w-100">
+                                    Follow us</p>
                                 <div class="social social-border">
                                     <ul class="d-flex list-unstyled">
                                         <li class="me-4 fs-4">
-                                            <a class="social-i" rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/company/triac-electronics-pvt-ltd/">
+                                            <a class="social-i" rel="noopener noreferrer" target="_blank"
+                                                href="https://www.linkedin.com/company/triac-electronics-pvt-ltd/">
                                                 <i class="fab fa-linkedin"></i>
                                             </a>
                                         </li>
@@ -90,7 +117,8 @@
                                 </div>
                             </div>
                             <div class="">
-                                <a class="btn  fs-5 contact-btn-foot" style="" title="Contact Us" href="contact.php">Contact Us</a>
+                                <a class="btn  fs-5 contact-btn-foot" style="" title="Contact Us"
+                                    href="contact.php">Contact Us</a>
                             </div>
                         </div>
                     </div>
@@ -103,10 +131,13 @@
 
     <div class="container text-center text-lg-right pt-0 pt-lg-5 ">
         <div class="">
-            <ul style="border-top: 1px solid #6c757d ;" class="d-flex align-items-center justify-content-end mb-0 list-unstyled pt-3">
+            <ul style="border-top: 1px solid #6c757d ;"
+                class="d-flex align-items-center justify-content-end mb-0 list-unstyled pt-3">
                 <li style="font-size: 13px;" class="ms-3 text-secondary">© 2022 Triac Electronics</li>
-                <li style="font-size: 13px;" class="ms-3"><a class="text-secondary" href="/privacy-policy/">Privacy Policy</a></li>
-                <li style="font-size: 13px;" class="ms-3"><a class="text-secondary" href="/terms-and-condition-of-sale">Terms and Conditions</a></li>
+                <li style="font-size: 13px;" class="ms-3"><a class="text-secondary" href="/privacy-policy/">Privacy
+                        Policy</a></li>
+                <li style="font-size: 13px;" class="ms-3"><a class="text-secondary"
+                        href="/terms-and-condition-of-sale">Terms and Conditions</a></li>
                 <li style="font-size: 13px;" class="ms-3"><a class="text-secondary" href="/site-map/">Site Map</a></li>
             </ul>
 
@@ -115,6 +146,8 @@
 
 </footer>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"
+    integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="js/All-Product-slider.js"></script>
 <script src="js/script.js"></script>
